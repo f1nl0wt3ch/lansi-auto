815eba19b30cb81c3ccc96333ede3b644df1bb9c
agolovin <alexgolovinoff@gmail.com>
2019-12-03 14:58:47 +0200

    Main Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import HomePage from './components/HomePageComponent/HomePage.js'
+import React, {Component} from 'react';
+import Navigation from "./Navigation";
+import Footer from "./Footer";
+import ServicePage from "./ServicePage";
+import MainPage from "./MainPage";
+import {
+    BrowserRouter as Router,
+    Route
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-       <HomePage />
-    </div>
-  );
+export default class App extends Component {
+    render() {
+        return (
+            <Router path="/">
+                <Navigation/>
+                <Route path="/" exact component={() => <MainPage/>}/>
+                <Route path="/services" component={() => <ServicePage/>}/>
+                <Footer/>
+            </Router>
+        )
+    }
 }
-export default App;
